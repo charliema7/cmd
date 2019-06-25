c3b081324d097784ff0ed491709849dd13fb7b8a
Charles Ma <charma@hotmail.com>
2019-06-24 19:02:29 -0700

    Install Devise Setup Soft Delete Setup User Management

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :trackable
+
+  # instead of deleting, indicate the user requested a delete & timestamp it  
+  def soft_delete
+    update_attribute(:deleted_at, Time.current)
+  end
+  
+  # ensure user account is active  
+  def active_for_authentication?
+    super && !deleted_at
+  end
+  
+  # provide a custom message for a deleted account
+  def inactive_message
+    !deleted_at ? super : :deleted_account
+  end
+end
